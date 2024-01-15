c93df081d166f1554f2d91c10fc83f70ff434e8f
dogth <dogth@kitteth.com>
2024-01-15 08:15:17 +0300

    finishing up

--- temp/series/series20.cpp
+++
@@ -1,8 +0,0 @@
-std::pair<std::vector<int>,int> solve(std::vector<int> numbers) {
-  std::vector<int> result;
-  auto iter = numbers.begin();
-  for (auto & number : numbers) {
-    number <= next(number) ? result.push_back(number):void();
-  }
-  return std::make_pair(result, result.size());
-}
--- temp/series/series8.cpp
+++
@@ -1,7 +0,0 @@
-std::pair<std::vector<int>,int> solve(std::vector<int> numbers) {
-  std::vector<int> result;
-  for (auto & number : numbers) {
-    number % 2 ? result.push_back(number):void();
-  }
-  return std::make_pair(result, result.size());
-}
